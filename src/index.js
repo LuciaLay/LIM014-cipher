7ef761f9b708bc8e835fd7552d41c43a2ecd6bca
LuciaLay <luciaalayval@gmail.com>
2021-01-19 10:43:06 -0500

    DOM para manejar <form> y output coded

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 const alphabet = [
-    'A','B','C','D','E','F',
-    'G','H','I','J','K','L',
-    'M','N','O','P','Q','R',
-    'S','T','U','V','W','X',
-    'Y','Z'
+    "A","B","C","D","E","F",
+    "G","H","I","J","K","L",
+    "M","N","O","P","Q","R",
+    "S","T","U","V","W","X",
+    "Y","Z"
   ];
   
+const form = document.forms[0];
+const output = document.getElementById("output");
