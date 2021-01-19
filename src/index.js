f234e326bbe485d81fb780960692722728f583b6
LuciaLay <luciaalayval@gmail.com>
2021-01-19 10:34:19 -0500

    creacion de variable con array del alfabeto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const alphabet = [
+    'A','B','C','D','E','F',
+    'G','H','I','J','K','L',
+    'M','N','O','P','Q','R',
+    'S','T','U','V','W','X',
+    'Y','Z'
+  ];
+  
