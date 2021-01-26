d242ece8ec5cb858e362ec1661f1f3bf104b3b3c
LuciaLay <luciaalayval@gmail.com>
2021-01-25 19:06:29 -0500

    se agregó las constantes alphabet y las funciones encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,47 @@
+const alphabet = [
+  "A","B","C","D","E","F",
+  "G","H","I","J","K","L",
+  "M","N","O","P","Q","R",
+  "S","T","U","V","W","X",
+  "Y","Z" ];
+
+  const alphabetB = [
+    'Z','Y','X','W','V','U',
+    'T','S','R','Q','P','O',
+    'N','M','L','K','J','I',
+    'H','G','F','E','D','C',
+    'B','A'
+  ];
+
+
 const cipher = {
-  // ...
+
+  encode: function(char,offset) {
+     
+ 
+    const shift = Number(offset);
+    if (alphabet.includes(char.toUpperCase()))
+    { 
+        const position = alphabet.indexOf(char.toUpperCase());
+        const newPosition = (position + shift)%26;
+        return alphabet[newPosition] 
+      }
+      else { return char }
+      },
+ 
+
+decode: function(char,offset) {
+        const shiftB = Number(offset);
+        if (alphabetB.includes(char.toUpperCase()))
+       { 
+          const positionB = alphabetB.indexOf(char.toUpperCase());
+          const newPositionB = (positionB + shiftB)%26;
+          return alphabetB[newPositionB] 
+        }
+        else { return char }
+        }
+     
 };
+
 
 export default cipher;
